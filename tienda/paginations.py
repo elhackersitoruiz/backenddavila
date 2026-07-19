ddc6a9f546a8cbbb185af7dda66b8b242bf320b0
felipe <1445663@senati.pe>
2026-07-19 01:07:42 -0500

    type import de rest_framework

--- tienda/paginations.py
+++ tienda/paginations.py
@@ -1,4 +1,4 @@
-from rest.framework.pagination import PageNumberPagination
+from rest_framework.pagination import PageNumberPagination
 
 class ProductPagination(PageNumberPagination):
     page_size = 40  
